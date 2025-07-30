5f7f0f1bdaf49bf94c963521fc81c35b2bb0f3dd
Chandrashekar <mrchandrashekarchandan@gmail.com>
2025-07-30 20:03:27 +0530

    connectedtodb

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express=require("express")
 const env=require("dotenv").config()
+const mongoose = require("mongoose");
 const app=express()
 
 let port=process.env.PORT || 8080
+
+function connectToDB(){
+    try{
+        mongoose.connect("mongodb://127.0.0.1:27017/qspider");
+        console.log("✅ connected to DB");
+    }catch(err){
+        console.log("❌ Error in connecting to mongoDB",err.message);
+    }
+}
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
 app.listen(port,()=>{
+    connectToDB()
     console.log("server running on port 3000");
 })
